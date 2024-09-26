7ab3b920cbe5d6e44e077323eb38967e5090f52a
Dnyaneshwary04 <dnyaneshwarydolas@gmail.com.com>
2024-09-27 00:50:22 +0530

    Add test for returning books

--- src/test_library.py
+++ src/test_library.py
@@ -10,11 +10,26 @@ class Library_Testing(unittest.TestCase):
         self.Library.addbook(book)
         self.assertIn(book,self.Library.books)
         
-    def test_borrow_book(self):
+    def test_borrowbook(self):
         book = Book(isbn="23-978-5163-389",title="Object Oriented Programing in Java",author="Rick Halterman",publicationYear=2013)
         self.Library.addbook(book)
         self.Library.borrowbook("23-978-5163-389")
         self.assertFalse(book.available)
+        
+    def test_borrow_unavailable_book(self):
+        book = Book(isbn="23-978-5163-389",title="Object Oriented Programing in Java",author="Rick Halterman",publicationYear=2013)
+        self.Library.addbook(book)
+        self.Library.borrowbook("23-978-5163-389")
+        with self.assertRaises(Exception) as message:
+            self.Library.borrowbook("23-978-5163-389")
+        self.assertTrue('Book Not Available' in str(message.exception))
+        
+    def test_returnbook(self):
+        book = Book(isbn="23-978-5163-389",title="Object Oriented Programing in Java",author="Rick Halterman",publicationYear=2013)
+        self.Library.addbook(book)
+        self.Library.borrowbook("23-978-5163-389")
+        self.Library.returnbook("23-978-5163-389")
+        self.assertTrue(book.available)
         
 if __name__ == '__main__':
     unittest.main()
