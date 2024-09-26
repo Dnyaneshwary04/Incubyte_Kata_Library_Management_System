5d6ab0f40a2d8d94fb9f7356133c02c484021aa5
Dnyaneshwary04 <dnyaneshwarydolas@gmail.com.com>
2024-09-27 00:44:12 +0530

    Add test for borrowing books

--- src/test_library.py
+++ src/test_library.py
@@ -10,5 +10,11 @@ class Library_Testing(unittest.TestCase):
         self.Library.addbook(book)
         self.assertIn(book,self.Library.books)
         
+    def test_borrow_book(self):
+        book = Book(isbn="23-978-5163-389",title="Object Oriented Programing in Java",author="Rick Halterman",publicationYear=2013)
+        self.Library.addbook(book)
+        self.Library.borrowbook("23-978-5163-389")
+        self.assertFalse(book.available)
+        
 if __name__ == '__main__':
     unittest.main()
